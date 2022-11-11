1b0477341b70d7eb390c69c9e9fb31c20daa2c32
Kirill Bagrov <4up9ra@gmail.com>
2022-11-11 17:50:50 +0300

    STEP_8 fixed URL for JavaRushPostClientTest

--- src/test/java/com/github/javarushcommunity/jrtb/javarushclient/JavaRushPostClientTest.java
+++ src/test/java/com/github/javarushcommunity/jrtb/javarushclient/JavaRushPostClientTest.java
@@ -7,19 +7,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-//import static com.github.javarushcommunity.jrtb.javarushclient.JavaRushGroupClientTest.JAVARUSH_API_PATH;
+import static com.github.javarushcommunity.jrtb.javarushclient.JavaRushGroupClientTest.JAVARUSH_API_PATH;
 
 @DisplayName("Integration-level testing for JavaRushPostClient")
 class JavaRushPostClientTest {
 
-    private final JavaRushPostClient postClient = new JavaRushPostClientImpl("${javarush.api.path}");
+    private final JavaRushPostClient postClient = new JavaRushPostClientImpl(JAVARUSH_API_PATH);
 
     @Test
-    public void shouldProperlyGetNew32Posts() {
+    public void shouldProperlyGetNew15Posts() {
         //when
         List<PostInfo> newPosts = postClient.findNewPosts(30, 2935);
 
         //then
-        Assertions.assertEquals(32, newPosts.size());
+        Assertions.assertEquals(15, newPosts.size());
     }
 }
